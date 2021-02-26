d9545c0804f003199a2c881c12cf82aa62a49a1d
Pascal P <lacsap_666@yahoo.fr>
2021-02-26 15:12:32 +1300

    C03/w02: assign. 1

--- C03/common/utils.py
+++ C03/common/utils.py
@@ -2,6 +2,7 @@
 import functools
 import time
 
+
 ## decorator
 def time_it(fn):
     @functools.wraps(fn)
@@ -21,3 +22,13 @@ def assert_all(fn, coll, msg=""):
     else:
         assert all(map(fn, coll))
     return
+
+
+def get_numpy_data(data_sf, features, label):
+    data_sf['intercept'] = 1.
+    features = ['intercept', *features]
+    features_sf = data_sf[features]
+    feature_matrix = features_sf.to_numpy()
+    label_sarray = data_sf[label]
+    label_array = label_sarray.to_numpy()
+    return (feature_matrix, label_array)
